430e1069e2d47db28bc05b9985e143ce23c04622
Kreolwolf1 <ashylo@heliostech.hk>
2012-08-09 12:47:18 +0300

    create app

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+gem 'devise'
+gem "haml-rails"
+gem "mongoid", "~> 3.0.0"
+gem "cancan"
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoTemplates::Application.config.secret_token = '0c64ad2245fd2b87177fbde208602f23493eb0e458cfd9e816906e4824d7639924b5f80ea1ed4fabf616cda9142eae14ef4499364f578aa525e61016d9bc0a0c'
